6ad68a71bc8d546c3670b94a401bcae55b334765
parthsavaj <100223673+parthsavaj@users.noreply.github.com>
2024-07-18 17:39:14 -0700

    Delete backend directory

--- backend/students/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Student(models.Model):
-    studentId = models.AutoField(primary_key=True)
-    FirstName = models.CharField(max_length=100)
-    LastName = models.CharField(max_length=100)
-    RegistrationNo = models.CharField(max_length=100)
-    Email = models.CharField(max_length=100)
-    Course = models.CharField(max_length=100)
